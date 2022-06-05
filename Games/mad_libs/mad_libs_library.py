82ca049e5140c8fd000fbed6589d8169bfe0caea
favour-olumese <70489864+favour-olumese@users.noreply.github.com>
2022-06-05 05:21:41 +0100

    Mad Libs Game Library
    Contains lists of sentence to be used for mad libs game.

---
+++ Games/mad_libs/mad_libs_library.py
@@ -0,0 +1,17 @@
+# mad_libs_library.py - A python script containing list of sentences to be used in a Mad Libs game.
+
+mad_libs = ['The dog VERB to the NOUN, barked at the NOUN, and the NOUN was ADJECTIVE',
+            'The ADJECTIVE cloud caused a big ADJECTIVE, and all NOUN closed early',
+            'If you watch horror movies at night, you might have ADJECTIVE dreams and be VERB',
+            'Sleep early to VERB early, for it is in the ADVERB that you have more ADJECTIVE',
+            'When you are ADJECTIVE, walk away, and keep your NOUN shut, least you VERB something wrong',
+            'What you VERB first thing in the morning, determines your NOUN.',
+            'To be great in NOUN, you need to VERB hard at your NOUN.',
+            'Reading opens you to ADJECTIVE ideas, and makes you see the world ADVERB.',
+            'To receive, you must first VERB; for that is how the world VERB.',
+            'It take NOUN to build friendship.',
+            'Programming is ADJECTIVE.',
+            'The best time to VERB your life is when you are ADJECTIVE.',
+            'Fruits VERB the body.',
+            'Knowing oneself is one of first to great NOUN.',
+            'When you stop learning, you start VERB.']
