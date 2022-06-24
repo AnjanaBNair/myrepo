f01bacc33af25d5af03c6c5514cc523ea3e679b3
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-06-24 21:21:45 +0100

    Create zigzag.py
    Zigzag animation.

---
+++ zigzag.py
@@ -0,0 +1,28 @@
+# zigzag.py - Zigzag animation.
+
+import time
+import sys
+
+indent = 0  # How many spaces to indent.
+indentIncreasing = True   # Whether the indent is increasing or not.
+
+try:
+    while True:  # The main program loop
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(2)  # pause for 1/10 of a second.
+        if indentIncreasing:
+            #  increase the number of spaces:
+            indent = indent + 1
+            if indent == 20:
+                # Change direction
+                indentIncreasing = False
+        else:
+            # Decrease the number of spaces
+            indent = indent - 1
+            if indent == 0:
+                # Change direction
+                indentIncreasing = True
+except KeyboardInterrupt:
+    sys.exit()
+    
