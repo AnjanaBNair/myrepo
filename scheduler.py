ab7b3c2db616d7618f9c42a8846af64cbe56ea11
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-09-28 14:13:13 +0100

    Added comments.

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # scheduler.py - Randomly schedule a list of people for tasks within a given range of dates 
 # during week days and inputting details in a csv file.
 
@@ -33,26 +34,29 @@ while start_date <= end_date:
     day = start_date.strftime('%A')
     date = start_date.strftime('%d-%m-%y')
 
+    # Skip Saturdays and Sundays.
     if day == 'Saturday' or day == 'Sunday':
         start_date += delta
         continue
 
+    # Add data to respective dictionaries with days of the week as the keys.
     dictweek[day] = date
     dictwrite[day] = name[i]
     
-    # Enter row of csv file every Friday.
+    # Enter rows of csv file every Friday and leaving a row empty.
     if day == 'Friday':
         content_writer.writerow(dictweek)
         content_writer.writerow(dictwrite)
         content_writer.writerow({})
 
+        # Check if it is a new month.
         new_month = start_date.strftime('%B')
 
         if new_month != month:
             content_writer.writerow({'Monday': new_month})
             month = new_month
         
-        # Emptying the dictionaries.
+        # Emptying the dictionaries after writing the rows.
         dictwrite = {}
         dictweek = {}
     
@@ -62,8 +66,8 @@ while start_date <= end_date:
 
     # When all members have been given a date, reshuffle the list and start again.
     if i > (no_of_members - 1):
-        i = 0  # Start from beginning
-        random.shuffle(name)  # Reshuffling list after all members had worked
+        i = 0  # Start from beginning.
+        random.shuffle(name)  # Reshuffling list after all members had worked.
 
 # When we get to the last week in the range, and it does not end on a Friday,
 # we input the name and dates still in the dictionaries to the csv file.
